35b8301e753ce3c9e6c721d3c55075f74b309d86
anito <axelnitzschner@mac.com>
2020-02-25 16:44:20 +0100

    fixes

--- src/RestApi/Controllers/ProductMetas.php
+++ src/RestApi/Controllers/ProductMetas.php
@@ -90,7 +90,7 @@ class ProductMetas extends WC_REST_Products_Controller {
 		$meta = $request['id'];
 
 		$instance = \Spine_js_woo::instance();
-		$ids      = $instance()->get_products_from_meta( $meta );
+		$ids      = $instance->get_products_from_meta( $meta );
 
 		$objects = array();
 		foreach ( $ids as $id ) {
